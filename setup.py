5a9d51c70e7a8d3482063fd229be41530bdb68df
Alexander Golec <goleca@gmail.com>
2020-04-13 19:13:19 -0400

    Released version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='tda-api',
-    version='0.0.2',
+    version='0.0.3',
     author='Alex Golec',
     author_email='bottomless.septic.tank@gmail.com',
     description='An unofficial wrapper around the TD Ameritrade HTTP API.',
