dd54aba990c9dd55992b5989a26aa8ee2556650f
Alexander Golec <goleca@gmail.com>
2020-05-27 19:19:25 -0400

    Added version-based import control

--- tda/__init__.py
+++ tda/__init__.py
@@ -1,5 +1,12 @@
 from . import auth
 from . import client
-from . import streaming
 from . import orders
 from .version import version as __version__
+
+def __python_version_supports_streaming():
+    import sys
+    version_info = sys.version_info
+    return version_info.major > 3 and version_info.minor > 8
+
+if __python_version_supports_streaming():
+    from . import streaming
