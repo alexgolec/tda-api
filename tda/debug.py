57573aa1ab034a00ec622ec92817e49387734e02
Alexander Golec <goleca@gmail.com>
2020-06-09 19:02:52 -0400

    Added tests for debug utilities

--- tda/debug.py
+++ tda/debug.py
@@ -1,27 +1,46 @@
+import sys
 import tda
 
 
 class LogRedactor:
+    '''
+    Collects strings that should not be emitted and replaces them with safe 
+    placeholders.
+    '''
+
     def __init__(self):
         from collections import defaultdict
 
         self.redacted_strings = {}
-        self.labels = defaultdict(lambda: 1)
+        self.label_counts = defaultdict(int)
 
     def register(self, string, label):
+        '''
+        Registers a string that should not be emitted and the label with with 
+        which it should be replaced.
+        '''
         if string not in self.redacted_strings:
-            self.redacted_strings[string] = '<REDACTED {}{}>'.format(
-                label, '-{}'.format(self.labels[label]) if
-                self.labels[label] > 1 else '')
-            self.labels[label] += 1
+            self.label_counts[label] += 1
+            self.redacted_strings[string] = (label, self.label_counts[label])
 
     def redact(self, msg):
+        '''
+        Scans the string for secret strings and returns a sanitized version with 
+        the secrets replaced with placeholders.
+        '''
         for string, label in self.redacted_strings.items():
-            msg = msg.replace(string, label)
+            label, count = label
+            msg = msg.replace(string, '<REDACTED {}{}>'.format(
+                label, '-{}'.format(count) if
+                    self.label_counts[label] > 1 else ''))
         return msg
 
 
 def register_redactions_from_response(resp):
+    '''
+    Convenience method that calls ``register_redactions`` if resp represents a 
+    successful response. Note this method assumes that resp has a JSON contents.
+    '''
     if resp.ok:
         register_redactions(resp.json())
 
@@ -31,6 +50,10 @@ def register_redactions(obj, key_path=None,
             'auth', 'acl', 'displayname', 'id', 'key', 'token'],
         whitelisted=set([
             'requestid'])):
+    '''
+    Recursively iterates through the leaf elements of ``obj`` and registers 
+    elements with keys matching a blacklist with the global ``Redactor``.
+    '''
     if key_path is None:
         key_path = []
 
@@ -50,12 +73,29 @@ def register_redactions(obj, key_path=None,
             if last_key in whitelisted:
                 return
             elif any(bad in last_key for bad in bad_patterns):
+                print('redacting')
                 tda.LOG_REDACTOR.register(obj, '-'.join(key_path))
 
 
-def enable_bug_report_logging():
+def enable_bug_report_logging(output=sys.stderr, loggers=None):
+    '''
+    Turns on bug report logging. Will collect all logged output, redact out 
+    anything that should be kept secret, and emit the result at program exit.
+
+    Notes:
+     * This method does a **BEST EFFORT** redaction. Never share its output 
+       without verifying that all secret information is properly redacted.
+     * Because this function records all logged output, it has a performance 
+       penalty. It should not be called in production code.
+    '''
     import atexit
     import logging
+
+    if loggers is None:
+        loggers = (
+                tda.auth.get_logger(),
+                tda.client.get_logger(),
+                tda.streaming.get_logger())
 
     class RecordingHandler(logging.Handler):
         def __init__(self, *args, **kwargs):
@@ -69,13 +109,12 @@ def enable_bug_report_logging():
     handler.setFormatter(logging.Formatter(
         '[%(filename)s:%(lineno)s:%(funcName)s] %(message)s'))
 
-    for m in (tda.auth, tda.client, tda.streaming):
-        logger = m.get_logger()
+    for logger in loggers:
         logger.setLevel(logging.DEBUG)
         logger.addHandler(handler)
 
     def write_logs():
         for msg in handler.messages:
             msg = tda.LOG_REDACTOR.redact(msg)
-            print(msg)
+            print(msg, file=output)
     atexit.register(write_logs)
---
+++ tests/debug_test.py
@@ -0,0 +1,98 @@
+import atexit
+import io
+import logging
+import tda
+import unittest
+
+from tda.client import Client
+from tests.test_utils import no_duplicates
+from unittest.mock import ANY, MagicMock, Mock, patch
+
+class RedactorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.redactor = tda.debug.LogRedactor()
+
+    @no_duplicates
+    def test_no_redactions(self):
+        self.assertEqual('test message', self.redactor.redact('test message'))
+
+    @no_duplicates
+    def test_simple_redaction(self):
+        self.redactor.register('secret', 'SECRET')
+
+        self.assertEqual(
+                '<REDACTED SECRET> message',
+                self.redactor.redact('secret message'))
+
+    @no_duplicates
+    def test_multiple_registrations_same_string(self):
+        self.redactor.register('secret', 'SECRET')
+        self.redactor.register('secret', 'SECRET')
+
+        self.assertEqual(
+                '<REDACTED SECRET> message',
+                self.redactor.redact('secret message'))
+
+    @no_duplicates
+    def test_multiple_registrations_same_string_different_label(self):
+        self.redactor.register('secret-A', 'SECRET')
+        self.redactor.register('secret-B', 'SECRET')
+
+        self.assertEqual(
+                '<REDACTED SECRET-1> message <REDACTED SECRET-2>',
+                self.redactor.redact('secret-A message secret-B'))
+
+
+class RegisterRedactionsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.captured = io.StringIO()
+        self.logger = logging.getLogger('test')
+        tda.debug.enable_bug_report_logging(
+                output=self.captured, loggers=[self.logger])
+
+    @no_duplicates
+    def test_empty_string(self):
+        tda.debug.register_redactions('')
+
+    @no_duplicates
+    def test_empty_dict(self):
+        tda.debug.register_redactions({})
+
+    @no_duplicates
+    def test_empty_list(self):
+        tda.debug.register_redactions([])
+
+    @no_duplicates
+    def test_dict(self):
+        tda.debug.register_redactions(
+                {'accountId': '100001'}, 
+                bad_patterns=['id'])
+        tda.debug.register_redactions(
+                {'otherAccountId': '200002'}, 
+                bad_patterns=['id'])
+
+        self.logger.info('Account ID: 100001')
+        self.logger.info('Other Account ID: 200002')
+
+        atexit._run_exitfuncs()
+        self.assertRegex(
+                self.captured.getvalue(),
+                r'\[.*\] Account ID: <REDACTED accountId>\n'+
+                r'\[.*\] Other Account ID: <REDACTED otherAccountId>\n')
+
+    @no_duplicates
+    def test_list_of_dict(self):
+        tda.debug.register_redactions(
+                [{'accountId': '100001'}, {'otherAccountId': '200002'}],
+                bad_patterns=['id'])
+
+        self.logger.info('Account ID: 100001')
+        self.logger.info('Other Account ID: 200002')
+
+        atexit._run_exitfuncs()
+        self.assertRegex(
+                self.captured.getvalue(),
+                r'\[.*\] Account ID: <REDACTED accountId>\n'+
+                r'\[.*\] Other Account ID: <REDACTED otherAccountId>\n')
