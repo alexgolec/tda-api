6f023d2cb36e0ab9c8392ea7b2a6d124286dd924
Alexander Golec <goleca@gmail.com>
2020-06-09 22:45:39 -0400

    Turns out calling atexit handlers prematurely messes with coverage reports

--- tda/debug.py
+++ tda/debug.py
@@ -4,7 +4,7 @@ import tda
 
 class LogRedactor:
     '''
-    Collects strings that should not be emitted and replaces them with safe 
+    Collects strings that should not be emitted and replaces them with safe
     placeholders.
     '''
 
@@ -16,7 +16,7 @@ class LogRedactor:
 
     def register(self, string, label):
         '''
-        Registers a string that should not be emitted and the label with with 
+        Registers a string that should not be emitted and the label with with
         which it should be replaced.
         '''
         if string not in self.redacted_strings:
@@ -25,20 +25,20 @@ class LogRedactor:
 
     def redact(self, msg):
         '''
-        Scans the string for secret strings and returns a sanitized version with 
+        Scans the string for secret strings and returns a sanitized version with
         the secrets replaced with placeholders.
         '''
         for string, label in self.redacted_strings.items():
             label, count = label
             msg = msg.replace(string, '<REDACTED {}{}>'.format(
                 label, '-{}'.format(count) if
-                    self.label_counts[label] > 1 else ''))
+                self.label_counts[label] > 1 else ''))
         return msg
 
 
 def register_redactions_from_response(resp):
     '''
-    Convenience method that calls ``register_redactions`` if resp represents a 
+    Convenience method that calls ``register_redactions`` if resp represents a
     successful response. Note this method assumes that resp has a JSON contents.
     '''
     if resp.ok:
@@ -46,12 +46,12 @@ def register_redactions_from_response(resp):
 
 
 def register_redactions(obj, key_path=None,
-        bad_patterns=[
-            'auth', 'acl', 'displayname', 'id', 'key', 'token'],
-        whitelisted=set([
-            'requestid'])):
+                        bad_patterns=[
+                            'auth', 'acl', 'displayname', 'id', 'key', 'token'],
+                        whitelisted=set([
+                            'requestid'])):
     '''
-    Recursively iterates through the leaf elements of ``obj`` and registers 
+    Recursively iterates through the leaf elements of ``obj`` and registers
     elements with keys matching a blacklist with the global ``Redactor``.
     '''
     if key_path is None:
@@ -73,29 +73,35 @@ def register_redactions(obj, key_path=None,
             if last_key in whitelisted:
                 return
             elif any(bad in last_key for bad in bad_patterns):
-                print('redacting')
                 tda.LOG_REDACTOR.register(obj, '-'.join(key_path))
 
 
 def enable_bug_report_logging(output=sys.stderr, loggers=None):
     '''
-    Turns on bug report logging. Will collect all logged output, redact out 
+    Turns on bug report logging. Will collect all logged output, redact out
     anything that should be kept secret, and emit the result at program exit.
 
     Notes:
-     * This method does a **BEST EFFORT** redaction. Never share its output 
+     * This method does a **BEST EFFORT** redaction. Never share its output
        without verifying that all secret information is properly redacted.
-     * Because this function records all logged output, it has a performance 
+     * Because this function records all logged output, it has a performance
        penalty. It should not be called in production code.
+
+    :param output: File to which output will be written. Defaults to ``stderr``.
+    :param loggers: List of loggers to register. Primarily available for 
+                    testing, so passing non-``None`` values is highly
+                    discouraged.
+
+    :return: Return value should be ignored.
     '''
     import atexit
     import logging
 
     if loggers is None:
         loggers = (
-                tda.auth.get_logger(),
-                tda.client.get_logger(),
-                tda.streaming.get_logger())
+            tda.auth.get_logger(),
+            tda.client.get_logger(),
+            tda.streaming.get_logger())
 
     class RecordingHandler(logging.Handler):
         def __init__(self, *args, **kwargs):
@@ -118,3 +124,5 @@ def enable_bug_report_logging(output=sys.stderr, loggers=None):
             msg = tda.LOG_REDACTOR.redact(msg)
             print(msg, file=output)
     atexit.register(write_logs)
+
+    return write_logs
--- tests/debug_test.py
+++ tests/debug_test.py
@@ -8,6 +8,7 @@ from tda.client import Client
 from tests.test_utils import no_duplicates
 from unittest.mock import ANY, MagicMock, Mock, patch
 
+
 class RedactorTest(unittest.TestCase):
 
     def setUp(self):
@@ -22,8 +23,8 @@ class RedactorTest(unittest.TestCase):
         self.redactor.register('secret', 'SECRET')
 
         self.assertEqual(
-                '<REDACTED SECRET> message',
-                self.redactor.redact('secret message'))
+            '<REDACTED SECRET> message',
+            self.redactor.redact('secret message'))
 
     @no_duplicates
     def test_multiple_registrations_same_string(self):
@@ -31,8 +32,8 @@ class RedactorTest(unittest.TestCase):
         self.redactor.register('secret', 'SECRET')
 
         self.assertEqual(
-                '<REDACTED SECRET> message',
-                self.redactor.redact('secret message'))
+            '<REDACTED SECRET> message',
+            self.redactor.redact('secret message'))
 
     @no_duplicates
     def test_multiple_registrations_same_string_different_label(self):
@@ -40,8 +41,8 @@ class RedactorTest(unittest.TestCase):
         self.redactor.register('secret-B', 'SECRET')
 
         self.assertEqual(
-                '<REDACTED SECRET-1> message <REDACTED SECRET-2>',
-                self.redactor.redact('secret-A message secret-B'))
+            '<REDACTED SECRET-1> message <REDACTED SECRET-2>',
+            self.redactor.redact('secret-A message secret-B'))
 
 
 class RegisterRedactionsTest(unittest.TestCase):
@@ -49,8 +50,8 @@ class RegisterRedactionsTest(unittest.TestCase):
     def setUp(self):
         self.captured = io.StringIO()
         self.logger = logging.getLogger('test')
-        tda.debug.enable_bug_report_logging(
-                output=self.captured, loggers=[self.logger])
+        self.dump_logs = tda.debug.enable_bug_report_logging(
+            output=self.captured, loggers=[self.logger])
 
     @no_duplicates
     def test_empty_string(self):
@@ -67,32 +68,32 @@ class RegisterRedactionsTest(unittest.TestCase):
     @no_duplicates
     def test_dict(self):
         tda.debug.register_redactions(
-                {'accountId': '100001'}, 
-                bad_patterns=['id'])
+            {'accountId': '100001'},
+            bad_patterns=['id'])
         tda.debug.register_redactions(
-                {'otherAccountId': '200002'}, 
-                bad_patterns=['id'])
+            {'otherAccountId': '200002'},
+            bad_patterns=['id'])
 
         self.logger.info('Account ID: 100001')
         self.logger.info('Other Account ID: 200002')
 
-        atexit._run_exitfuncs()
+        self.dump_logs()
         self.assertRegex(
-                self.captured.getvalue(),
-                r'\[.*\] Account ID: <REDACTED accountId>\n'+
-                r'\[.*\] Other Account ID: <REDACTED otherAccountId>\n')
+            self.captured.getvalue(),
+            r'\[.*\] Account ID: <REDACTED accountId>\n' +
+            r'\[.*\] Other Account ID: <REDACTED otherAccountId>\n')
 
     @no_duplicates
     def test_list_of_dict(self):
         tda.debug.register_redactions(
-                [{'accountId': '100001'}, {'otherAccountId': '200002'}],
-                bad_patterns=['id'])
+            [{'accountId': '100001'}, {'otherAccountId': '200002'}],
+            bad_patterns=['id'])
 
         self.logger.info('Account ID: 100001')
         self.logger.info('Other Account ID: 200002')
 
-        atexit._run_exitfuncs()
+        self.dump_logs()
         self.assertRegex(
-                self.captured.getvalue(),
-                r'\[.*\] Account ID: <REDACTED accountId>\n'+
-                r'\[.*\] Other Account ID: <REDACTED otherAccountId>\n')
+            self.captured.getvalue(),
+            r'\[.*\] Account ID: <REDACTED accountId>\n' +
+            r'\[.*\] Other Account ID: <REDACTED otherAccountId>\n')
