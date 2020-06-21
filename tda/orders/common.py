8c321be0bded31c3d392488f7778661c64409133
Alexander Golec <goleca@gmail.com>
2020-06-20 21:01:23 -0400

    Added tests for OCO and TRIGGER orders

--- tda/orders/common.py
+++ tda/orders/common.py
@@ -334,6 +334,8 @@ def one_cancels_other(order1, order2):
     '''
     If one of the orders is executed, immediately cancel the other.
     '''
+    from tda.orders.generic import OrderBuilder
+
     return (OrderBuilder()
             .set_order_strategy_type(OrderStrategyType.OCO)
             .add_child_order_strategy(order1)
@@ -344,6 +346,8 @@ def first_triggers_second(first_order, second_order):
     '''
     If ``first_order`` is executed, immediately place ``second_order``.
     '''
+    from tda.orders.generic import OrderBuilder
+
     return (first_order
             .set_order_strategy_type(OrderStrategyType.TRIGGER)
             .add_child_order_strategy(second_order))
---
+++ tests/orders/common_test.py
@@ -0,0 +1,31 @@
+from tests.test_utils import has_diff, no_duplicates
+from tda.orders.common import *
+from tda.orders.generic import OrderBuilder
+
+import unittest
+
+class MultiOrderTest(unittest.TestCase):
+
+    def test_oco(self):
+        self.assertFalse(has_diff({
+            'orderStrategyType': 'OCO',
+            'childOrderStrategies': [
+                {'session': 'NORMAL'},
+                {'duration': 'DAY'},
+            ]
+        }, one_cancels_other(
+            OrderBuilder().set_session(Session.NORMAL),
+            OrderBuilder().set_duration(Duration.DAY)).build()))
+
+    def test_trigger(self):
+        self.assertFalse(has_diff({
+            'orderStrategyType': 'TRIGGER',
+            'session': 'NORMAL',
+            'childOrderStrategies': [
+                {'duration': 'DAY'},
+            ]
+        }, first_triggers_second(
+            OrderBuilder().set_session(Session.NORMAL),
+            OrderBuilder().set_duration(Duration.DAY)).build()))
+
+
