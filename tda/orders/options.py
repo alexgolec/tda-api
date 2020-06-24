9cce4e100afd5e2f97a61d994d78e343c46e668d
Alexander Golec <goleca@gmail.com>
2020-06-23 23:19:43 -0400

    Adds basic option templates

---
+++ tda/orders/options.py
@@ -0,0 +1,151 @@
+from tda.orders.generic import OrderBuilder
+
+
+##########################################################################
+# Buy to Open
+
+def buy_option_to_open_market(symbol, quantity):
+    '''
+    Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
+    buy-to-open market order.
+    '''
+    from tda.orders.common import Duration, OptionInstruction, OrderType
+    from tda.orders.common import OrderStrategyType, Session
+
+    return (OrderBuilder()
+            .set_session(Session.NORMAL)
+            .set_duration(Duration.DAY)
+            .set_order_type(OrderType.MARKET)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(OptionInstruction.BUY_TO_OPEN, symbol, quantity))
+
+
+def buy_option_to_open_limit(symbol, quantity, price):
+    '''
+    Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
+    buy-to-open limit order.
+    '''
+    from tda.orders.common import Duration, OptionInstruction, OrderType
+    from tda.orders.common import OrderStrategyType, Session
+
+    return (OrderBuilder()
+            .set_session(Session.NORMAL)
+            .set_duration(Duration.DAY)
+            .set_order_type(OrderType.LIMIT)
+            .set_price(price)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(OptionInstruction.BUY_TO_OPEN, symbol, quantity)
+    )
+
+
+##########################################################################
+# Sell to Open
+
+def sell_option_to_open_market(symbol, quantity):
+    '''
+    Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
+    sell-to-open market order.
+    '''
+    from tda.orders.common import Duration, OptionInstruction, OrderType
+    from tda.orders.common import OrderStrategyType, Session
+
+    return (OrderBuilder()
+            .set_session(Session.NORMAL)
+            .set_duration(Duration.DAY)
+            .set_order_type(OrderType.MARKET)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(OptionInstruction.SELL_TO_OPEN, symbol, quantity))
+
+
+def sell_option_to_open_limit(symbol, quantity, price):
+    '''
+    Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
+    sell-to-open limit order.
+    '''
+    from tda.orders.common import Duration, OptionInstruction, OrderType
+    from tda.orders.common import OrderStrategyType, Session
+
+    return (OrderBuilder()
+            .set_session(Session.NORMAL)
+            .set_duration(Duration.DAY)
+            .set_order_type(OrderType.LIMIT)
+            .set_price(price)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(OptionInstruction.SELL_TO_OPEN, symbol, quantity)
+    )
+
+
+##########################################################################
+# Buy to Close
+
+
+def buy_option_to_close_market(symbol, quantity):
+    '''
+    Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
+    buy-to-close market order.
+    '''
+    from tda.orders.common import Duration, OptionInstruction, OrderType
+    from tda.orders.common import OrderStrategyType, Session
+
+    return (OrderBuilder()
+            .set_session(Session.NORMAL)
+            .set_duration(Duration.DAY)
+            .set_order_type(OrderType.MARKET)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(OptionInstruction.BUY_TO_CLOSE, symbol, quantity))
+
+
+def buy_option_to_close_limit(symbol, quantity, price):
+    '''
+    Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
+    buy-to-close limit order.
+    '''
+    from tda.orders.common import Duration, OptionInstruction, OrderType
+    from tda.orders.common import OrderStrategyType, Session
+
+    return (OrderBuilder()
+            .set_session(Session.NORMAL)
+            .set_duration(Duration.DAY)
+            .set_order_type(OrderType.LIMIT)
+            .set_price(price)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(OptionInstruction.BUY_TO_CLOSE, symbol, quantity)
+    )
+
+
+##########################################################################
+# Sell to Close
+
+
+def sell_option_to_close_market(symbol, quantity):
+    '''
+    Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
+    sell-to-close market order.
+    '''
+    from tda.orders.common import Duration, OptionInstruction, OrderType
+    from tda.orders.common import OrderStrategyType, Session
+
+    return (OrderBuilder()
+            .set_session(Session.NORMAL)
+            .set_duration(Duration.DAY)
+            .set_order_type(OrderType.MARKET)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(OptionInstruction.SELL_TO_CLOSE, symbol, quantity))
+
+
+def sell_option_to_close_limit(symbol, quantity, price):
+    '''
+    Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
+    sell-to-close limit order.
+    '''
+    from tda.orders.common import Duration, OptionInstruction, OrderType
+    from tda.orders.common import OrderStrategyType, Session
+
+    return (OrderBuilder()
+            .set_session(Session.NORMAL)
+            .set_duration(Duration.DAY)
+            .set_order_type(OrderType.LIMIT)
+            .set_price(price)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(OptionInstruction.SELL_TO_CLOSE, symbol, quantity)
+    )
