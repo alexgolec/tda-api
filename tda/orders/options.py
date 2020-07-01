01c0d463522f0b31c444d095cae430505f6bf70f
Alexander Golec <goleca@gmail.com>
2020-07-01 00:20:15 -0400

    Added tests for verticals

--- tda/orders/options.py
+++ tda/orders/options.py
@@ -1,7 +1,17 @@
 from tda.orders.generic import OrderBuilder
 
 
-##########################################################################
+def __base_builder():
+    from tda.orders.common import Duration, Session
+
+    return (OrderBuilder()
+            .set_session(Session.NORMAL)
+            .set_duration(Duration.DAY))
+
+
+################################################################################
+# Single options
+
 # Buy to Open
 
 def option_buy_to_open_market(symbol, quantity):
@@ -9,12 +19,9 @@ def option_buy_to_open_market(symbol, quantity):
     Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
     buy-to-open market order.
     '''
-    from tda.orders.common import Duration, OptionInstruction, OrderType
-    from tda.orders.common import OrderStrategyType, Session
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
 
-    return (OrderBuilder()
-            .set_session(Session.NORMAL)
-            .set_duration(Duration.DAY)
+    return (__base_builder()
             .set_order_type(OrderType.MARKET)
             .set_order_strategy_type(OrderStrategyType.SINGLE)
             .add_option_leg(OptionInstruction.BUY_TO_OPEN, symbol, quantity))
@@ -25,12 +32,9 @@ def option_buy_to_open_limit(symbol, quantity, price):
     Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
     buy-to-open limit order.
     '''
-    from tda.orders.common import Duration, OptionInstruction, OrderType
-    from tda.orders.common import OrderStrategyType, Session
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
 
-    return (OrderBuilder()
-            .set_session(Session.NORMAL)
-            .set_duration(Duration.DAY)
+    return (__base_builder()
             .set_order_type(OrderType.LIMIT)
             .set_price(price)
             .set_order_strategy_type(OrderStrategyType.SINGLE)
@@ -38,7 +42,6 @@ def option_buy_to_open_limit(symbol, quantity, price):
     )
 
 
-##########################################################################
 # Sell to Open
 
 def option_sell_to_open_market(symbol, quantity):
@@ -46,12 +49,9 @@ def option_sell_to_open_market(symbol, quantity):
     Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
     sell-to-open market order.
     '''
-    from tda.orders.common import Duration, OptionInstruction, OrderType
-    from tda.orders.common import OrderStrategyType, Session
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
 
-    return (OrderBuilder()
-            .set_session(Session.NORMAL)
-            .set_duration(Duration.DAY)
+    return (__base_builder()
             .set_order_type(OrderType.MARKET)
             .set_order_strategy_type(OrderStrategyType.SINGLE)
             .add_option_leg(OptionInstruction.SELL_TO_OPEN, symbol, quantity))
@@ -62,12 +62,9 @@ def option_sell_to_open_limit(symbol, quantity, price):
     Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
     sell-to-open limit order.
     '''
-    from tda.orders.common import Duration, OptionInstruction, OrderType
-    from tda.orders.common import OrderStrategyType, Session
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
 
-    return (OrderBuilder()
-            .set_session(Session.NORMAL)
-            .set_duration(Duration.DAY)
+    return (__base_builder()
             .set_order_type(OrderType.LIMIT)
             .set_price(price)
             .set_order_strategy_type(OrderStrategyType.SINGLE)
@@ -75,7 +72,6 @@ def option_sell_to_open_limit(symbol, quantity, price):
     )
 
 
-##########################################################################
 # Buy to Close
 
 
@@ -84,12 +80,9 @@ def option_buy_to_close_market(symbol, quantity):
     Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
     buy-to-close market order.
     '''
-    from tda.orders.common import Duration, OptionInstruction, OrderType
-    from tda.orders.common import OrderStrategyType, Session
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
 
-    return (OrderBuilder()
-            .set_session(Session.NORMAL)
-            .set_duration(Duration.DAY)
+    return (__base_builder()
             .set_order_type(OrderType.MARKET)
             .set_order_strategy_type(OrderStrategyType.SINGLE)
             .add_option_leg(OptionInstruction.BUY_TO_CLOSE, symbol, quantity))
@@ -100,12 +93,9 @@ def option_buy_to_close_limit(symbol, quantity, price):
     Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
     buy-to-close limit order.
     '''
-    from tda.orders.common import Duration, OptionInstruction, OrderType
-    from tda.orders.common import OrderStrategyType, Session
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
 
-    return (OrderBuilder()
-            .set_session(Session.NORMAL)
-            .set_duration(Duration.DAY)
+    return (__base_builder
             .set_order_type(OrderType.LIMIT)
             .set_price(price)
             .set_order_strategy_type(OrderStrategyType.SINGLE)
@@ -113,7 +103,6 @@ def option_buy_to_close_limit(symbol, quantity, price):
     )
 
 
-##########################################################################
 # Sell to Close
 
 
@@ -122,12 +111,9 @@ def option_sell_to_close_market(symbol, quantity):
     Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
     sell-to-close market order.
     '''
-    from tda.orders.common import Duration, OptionInstruction, OrderType
-    from tda.orders.common import OrderStrategyType, Session
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
 
-    return (OrderBuilder()
-            .set_session(Session.NORMAL)
-            .set_duration(Duration.DAY)
+    return (__base_builder()
             .set_order_type(OrderType.MARKET)
             .set_order_strategy_type(OrderStrategyType.SINGLE)
             .add_option_leg(OptionInstruction.SELL_TO_CLOSE, symbol, quantity))
@@ -138,14 +124,155 @@ def option_sell_to_close_limit(symbol, quantity, price):
     Returns a pre-filled :class:`~tda.orders.generic.OrderBuilder` for a
     sell-to-close limit order.
     '''
-    from tda.orders.common import Duration, OptionInstruction, OrderType
-    from tda.orders.common import OrderStrategyType, Session
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
 
-    return (OrderBuilder()
-            .set_session(Session.NORMAL)
-            .set_duration(Duration.DAY)
+    return (__base_builder()
             .set_order_type(OrderType.LIMIT)
             .set_price(price)
             .set_order_strategy_type(OrderStrategyType.SINGLE)
             .add_option_leg(OptionInstruction.SELL_TO_CLOSE, symbol, quantity)
     )
+
+
+################################################################################
+# Verticals
+
+# Bull Call
+
+def bull_call_vertical_open(
+        long_call_symbol, short_call_symbol, quantity, net_debit):
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
+    from tda.orders.common import ComplexOrderStrategyType
+
+    return (__base_builder()
+            .set_order_type(OrderType.NET_DEBIT)
+            .set_complex_order_strategy_type(ComplexOrderStrategyType.VERTICAL)
+            .set_price(net_debit)
+            .set_quantity(quantity)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(
+                OptionInstruction.BUY_TO_OPEN, long_call_symbol, quantity)
+            .add_option_leg(
+                OptionInstruction.SELL_TO_OPEN, short_call_symbol, quantity))
+
+def bull_call_vertical_close(
+        long_call_symbol, short_call_symbol, quantity, net_credit):
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
+    from tda.orders.common import ComplexOrderStrategyType
+
+    return (__base_builder()
+            .set_order_type(OrderType.NET_CREDIT)
+            .set_complex_order_strategy_type(ComplexOrderStrategyType.VERTICAL)
+            .set_price(net_credit)
+            .set_quantity(quantity)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(
+                OptionInstruction.SELL_TO_CLOSE, long_call_symbol, quantity)
+            .add_option_leg(
+                OptionInstruction.BUY_TO_CLOSE, short_call_symbol, quantity))
+
+
+# Bear Call
+
+def bear_call_vertical_open(
+        short_call_symbol, long_call_symbol, quantity, net_credit):
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
+    from tda.orders.common import ComplexOrderStrategyType
+
+    return (__base_builder()
+            .set_order_type(OrderType.NET_CREDIT)
+            .set_complex_order_strategy_type(ComplexOrderStrategyType.VERTICAL)
+            .set_price(net_credit)
+            .set_quantity(quantity)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(
+                OptionInstruction.SELL_TO_OPEN, short_call_symbol, quantity)
+            .add_option_leg(
+                OptionInstruction.BUY_TO_OPEN, long_call_symbol, quantity))
+
+def bear_call_vertical_close(
+        short_call_symbol, long_call_symbol, quantity, net_debit):
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
+    from tda.orders.common import ComplexOrderStrategyType
+
+    return (__base_builder()
+            .set_order_type(OrderType.NET_DEBIT)
+            .set_complex_order_strategy_type(ComplexOrderStrategyType.VERTICAL)
+            .set_price(net_debit)
+            .set_quantity(quantity)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(
+                OptionInstruction.BUY_TO_CLOSE, short_call_symbol, quantity)
+            .add_option_leg(
+                OptionInstruction.SELL_TO_CLOSE, long_call_symbol, quantity))
+
+
+# Bull Put
+
+def bull_put_vertical_open(
+        long_put_symbol, short_put_symbol, quantity, net_credit):
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
+    from tda.orders.common import ComplexOrderStrategyType
+
+    return (__base_builder()
+            .set_order_type(OrderType.NET_CREDIT)
+            .set_complex_order_strategy_type(ComplexOrderStrategyType.VERTICAL)
+            .set_price(net_credit)
+            .set_quantity(quantity)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(
+                OptionInstruction.BUY_TO_OPEN, long_put_symbol, quantity)
+            .add_option_leg(
+                OptionInstruction.SELL_TO_OPEN, short_put_symbol, quantity))
+
+def bull_put_vertical_close(
+        long_put_symbol, short_put_symbol, quantity, net_debit):
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
+    from tda.orders.common import ComplexOrderStrategyType
+
+    return (__base_builder()
+            .set_order_type(OrderType.NET_DEBIT)
+            .set_complex_order_strategy_type(ComplexOrderStrategyType.VERTICAL)
+            .set_price(net_debit)
+            .set_quantity(quantity)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(
+                OptionInstruction.SELL_TO_CLOSE, long_put_symbol, quantity)
+            .add_option_leg(
+                OptionInstruction.BUY_TO_CLOSE, short_put_symbol, quantity))
+
+
+# Bull Call
+
+def bear_put_vertical_open(
+        short_put_symbol, long_put_symbol, quantity, net_debit):
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
+    from tda.orders.common import ComplexOrderStrategyType
+
+    return (__base_builder()
+            .set_order_type(OrderType.NET_DEBIT)
+            .set_complex_order_strategy_type(ComplexOrderStrategyType.VERTICAL)
+            .set_price(net_debit)
+            .set_quantity(quantity)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(
+                OptionInstruction.SELL_TO_OPEN, short_put_symbol, quantity)
+            .add_option_leg(
+                OptionInstruction.BUY_TO_OPEN, long_put_symbol, quantity))
+
+def bear_put_vertical_close(
+        short_put_symbol, long_put_symbol, quantity, net_credit):
+    from tda.orders.common import OptionInstruction, OrderType, OrderStrategyType
+    from tda.orders.common import ComplexOrderStrategyType
+
+    return (__base_builder()
+            .set_order_type(OrderType.NET_CREDIT)
+            .set_complex_order_strategy_type(ComplexOrderStrategyType.VERTICAL)
+            .set_price(net_credit)
+            .set_quantity(quantity)
+            .set_order_strategy_type(OrderStrategyType.SINGLE)
+            .add_option_leg(
+                OptionInstruction.BUY_TO_CLOSE, short_put_symbol, quantity)
+            .add_option_leg(
+                OptionInstruction.SELL_TO_CLOSE, long_put_symbol, quantity))
+
--- tests/orders/options_test.py
+++ tests/orders/options_test.py
@@ -148,3 +148,239 @@ class OptionTemplatesTest(unittest.TestCase):
         }, option_sell_to_close_limit('GOOG_012122P2200', 10, 32.5).build()))
 
 
+
+class VerticalTemplatesTest(unittest.TestCase):
+
+    def test_bull_call_vertical_open(self):
+        self.assertFalse(has_diff({
+            'orderType': 'NET_DEBIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'price': '30.60',
+            'complexOrderStrategyType': 'VERTICAL',
+            'quantity': 3,
+            'orderLegCollection': [{
+                'instruction': 'BUY_TO_OPEN',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122C2200',
+                    'assetType': 'OPTION',
+                }
+            }, {
+                'instruction': 'SELL_TO_OPEN',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122C2400',
+                    'assetType': 'OPTION',
+                }
+            }]
+        }, bull_call_vertical_open(
+            'GOOG_012122C2200',
+            'GOOG_012122C2400',
+            3, 30.6).build()))
+
+    def test_bull_call_vertical_close(self):
+        self.assertFalse(has_diff({
+            'orderType': 'NET_CREDIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'price': '30.60',
+            'complexOrderStrategyType': 'VERTICAL',
+            'quantity': 3,
+            'orderLegCollection': [{
+                'instruction': 'SELL_TO_CLOSE',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122C2200',
+                    'assetType': 'OPTION',
+                }
+            }, {
+                'instruction': 'BUY_TO_CLOSE',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122C2400',
+                    'assetType': 'OPTION',
+                }
+            }]
+        }, bull_call_vertical_close(
+            'GOOG_012122C2200',
+            'GOOG_012122C2400',
+            3, 30.6).build()))
+
+    def test_bear_call_vertical_open(self):
+        self.assertFalse(has_diff({
+            'orderType': 'NET_CREDIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'price': '30.60',
+            'complexOrderStrategyType': 'VERTICAL',
+            'quantity': 3,
+            'orderLegCollection': [{
+                'instruction': 'SELL_TO_OPEN',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122C2200',
+                    'assetType': 'OPTION',
+                }
+            }, {
+                'instruction': 'BUY_TO_OPEN',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122C2400',
+                    'assetType': 'OPTION',
+                }
+            }]
+        }, bear_call_vertical_open(
+            'GOOG_012122C2200',
+            'GOOG_012122C2400',
+            3, 30.6).build()))
+
+    def test_bear_call_vertical_close(self):
+        self.assertFalse(has_diff({
+            'orderType': 'NET_DEBIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'price': '30.60',
+            'complexOrderStrategyType': 'VERTICAL',
+            'quantity': 3,
+            'orderLegCollection': [{
+                'instruction': 'BUY_TO_CLOSE',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122C2200',
+                    'assetType': 'OPTION',
+                }
+            }, {
+                'instruction': 'SELL_TO_CLOSE',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122C2400',
+                    'assetType': 'OPTION',
+                }
+            }]
+        }, bear_call_vertical_close(
+            'GOOG_012122C2200',
+            'GOOG_012122C2400',
+            3, 30.6).build()))
+
+    def test_bull_put_vertical_open(self):
+        self.assertFalse(has_diff({
+            'orderType': 'NET_CREDIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'price': '30.60',
+            'complexOrderStrategyType': 'VERTICAL',
+            'quantity': 3,
+            'orderLegCollection': [{
+                'instruction': 'BUY_TO_OPEN',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122P2200',
+                    'assetType': 'OPTION',
+                }
+            }, {
+                'instruction': 'SELL_TO_OPEN',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122P2400',
+                    'assetType': 'OPTION',
+                }
+            }]
+        }, bull_put_vertical_open(
+            'GOOG_012122P2200',
+            'GOOG_012122P2400',
+            3, 30.6).build()))
+
+    def test_bull_put_vertical_close(self):
+        self.assertFalse(has_diff({
+            'orderType': 'NET_DEBIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'price': '30.60',
+            'complexOrderStrategyType': 'VERTICAL',
+            'quantity': 3,
+            'orderLegCollection': [{
+                'instruction': 'SELL_TO_CLOSE',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122P2200',
+                    'assetType': 'OPTION',
+                }
+            }, {
+                'instruction': 'BUY_TO_CLOSE',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122P2400',
+                    'assetType': 'OPTION',
+                }
+            }]
+        }, bull_put_vertical_close(
+            'GOOG_012122P2200',
+            'GOOG_012122P2400',
+            3, 30.6).build()))
+
+    def test_bear_put_vertical_open(self):
+        self.assertFalse(has_diff({
+            'orderType': 'NET_DEBIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'price': '30.60',
+            'complexOrderStrategyType': 'VERTICAL',
+            'quantity': 3,
+            'orderLegCollection': [{
+                'instruction': 'SELL_TO_OPEN',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122P2200',
+                    'assetType': 'OPTION',
+                }
+            }, {
+                'instruction': 'BUY_TO_OPEN',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122P2400',
+                    'assetType': 'OPTION',
+                }
+            }]
+        }, bear_put_vertical_open(
+            'GOOG_012122P2200',
+            'GOOG_012122P2400',
+            3, 30.6).build()))
+
+    def test_bear_put_vertical_close(self):
+        self.assertFalse(has_diff({
+            'orderType': 'NET_CREDIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'price': '30.60',
+            'complexOrderStrategyType': 'VERTICAL',
+            'quantity': 3,
+            'orderLegCollection': [{
+                'instruction': 'BUY_TO_CLOSE',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122P2200',
+                    'assetType': 'OPTION',
+                }
+            }, {
+                'instruction': 'SELL_TO_CLOSE',
+                'quantity': 3,
+                'instrument': {
+                    'symbol': 'GOOG_012122P2400',
+                    'assetType': 'OPTION',
+                }
+            }]
+        }, bear_put_vertical_close(
+            'GOOG_012122P2200',
+            'GOOG_012122P2400',
+            3, 30.6).build()))
+
+
