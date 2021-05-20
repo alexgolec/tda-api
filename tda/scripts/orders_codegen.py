56f29b53c76b4591714084888394fd7e7fa86d85
Alex Golec <goleca@gmail.com>
2021-05-20 08:31:11 -0400

    Replaced complicated orders regeneration script with a simple 'recreate latest order' one

--- tda/scripts/orders_codegen.py
+++ tda/scripts/orders_codegen.py
@@ -1,9 +1,11 @@
 import argparse
+import json
 
 from tda.auth import client_from_token_file
 from tda.contrib.orders import construct_repeat_order, code_for_builder
 
-def main(sys_args):
+
+def latest_order_main(sys_args):
     parser = argparse.ArgumentParser(
             description='Utilities for generating code from historical orders')
 
@@ -14,39 +16,27 @@ def main(sys_args):
 
     parser.add_argument('--account_id', type=int,
             help='Restrict lookups to a specific account ID')
-    parser.add_argument('--order_id', type=int,
-            help='If set, generate code for only this order ID. If unset, '+
-                 'generate code for all historical orders.')
 
     args = parser.parse_args(args=sys_args)
-
     client = client_from_token_file(args.token_file, args.api_key)
 
-    order_id = args.order_id
-    account_id = args.account_id
-
-    if order_id:
-        if account_id:
-            orders = [client.get_order(order_id, account_id).json()]
-        else:
-            accounts = client.get_accounts().json()
-            for account in accounts:
-                account_id = account['securitiesAccount']['accountId']
-                order = client.get_order(order_id, account_id).json()
-                if 'error' not in order:
-                    orders = [order]
-                    break
-            else:
-                print('Failed to find order for order_id', order_id)
-                return -1
+    if args.account_id:
+        orders = client.get_orders_by_path(args.account_id).json()
+        if 'error' in orders:
+            print(('TDA returned error: "{}", This is most often caused by ' +
+                   'an invalid account ID').format(orders['error']))
+            return -1
     else:
-        if account_id:
-            orders = client.get_orders_by_path(account_id).json()
-        else:
-            orders = client.get_orders_by_query().json()
+        orders = client.get_orders_by_query().json()
+        if 'error' in orders:
+            print('TDA returned error: "{}"'.format(orders['error']))
+            return -1
 
-    for order in sorted(orders, key=lambda order: order['orderId']):
+    if orders:
+        order = sorted(orders, key=lambda o: -o['orderId'])[0]
         print('# Order ID', order['orderId'])
         print(code_for_builder(construct_repeat_order(order)))
+    else:
+        print('No recent orders found')
 
     return 0
