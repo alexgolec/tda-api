0fec80157f426d56111cbb99f5b921cb25ae8713
Alex Golec <goleca@gmail.com>
2021-08-09 16:46:56 -0400

    Clarifies comment on need_extended_hours_data

--- tda/client/base.py
+++ tda/client/base.py
@@ -769,8 +769,7 @@ class BaseClient(EnumEnforcer):
         :param start_datetime: Start date.
         :param end_datetime: End date. Default is previous trading day.
         :param need_extended_hours_data: If true, return extended hours data.
-                                         Otherwise return regular market hours
-                                         only.
+                                         Default is true.
         '''
         period_type = self.convert_enum(
             period_type, self.PriceHistory.PeriodType)
